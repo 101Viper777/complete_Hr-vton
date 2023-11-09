2b58a38f9041edf8410d830ae4e93c1d1df587db
Abdul <127184231+101Viper777@users.noreply.github.com>
2023-11-09 13:33:34 +1100

    up

--- get_cloth_mask.py
+++ get_cloth_mask.py
@@ -21,24 +21,37 @@ x = transform(image=padded_image)["image"]
 x = torch.unsqueeze(tensor_from_rgb_image(x), 0)
 
 with torch.no_grad():
-  prediction = model(x)[0][0]
+    prediction = model(x)[0][0]
 
 mask = (prediction > 0).cpu().numpy().astype(np.uint8)
 mask = unpad(mask, pads)
 
-img=np.full((1024,768,3), 255)
-seg_img=np.full((1024,768), 0)
+# Initialize the destination images
+img = np.full((1024, 768, 3), 255, dtype=np.uint8)
+seg_img = np.full((1024, 768), 0, dtype=np.uint8)
 
-b=cv2.imread("./static/cloth_web.jpg")
-b_img = mask* 255
-                 
-if b.shape[1]<=600 and b.shape[0]<=500:
-    b=cv2.resize(b,(int(b.shape[1]*1.2),int(b.shape[0]*1.2)))
-    b_img=cv2.resize(b_img,(int(b_img.shape[1]*1.2),int(b_img.shape[0]*1.2)))
-shape=b_img.shape
-img[int((1024-shape[0])/2): 1024-int((1024-shape[0])/2),int((768-shape[1])/2):768-int((768-shape[1])/2)]=b
-seg_img[int((1024-shape[0])/2): 1024-int((1024-shape[0])/2),int((768-shape[1])/2):768-int((768-shape[1])/2)]=b_img
+# Read the original image
+b = cv2.imread("./static/cloth_web.jpg")
+b_img = mask * 255
 
+# Resize if needed
+if b.shape[1] <= 600 and b.shape[0] <= 500:
+    b = cv2.resize(b, (int(b.shape[1] * 1.2), int(b.shape[0] * 1.2)))
+    b_img = cv2.resize(b_img, (int(b_img.shape[1] * 1.2), int(b_img.shape[0] * 1.2)))
 
-cv2.imwrite("./HR-VITON-main/test/test/cloth/00001_00.jpg",img)
-cv2.imwrite("./HR-VITON-main/test/test/cloth-mask/00001_00.jpg",seg_img)
+# Compute the start and end indices for height and width to avoid rounding issues
+height, width, _ = b.shape
+start_h = int((1024 - height) // 2)
+end_h = start_h + height
+start_w = int((768 - width) // 2)
+end_w = start_w + width
+
+# Place `b` into `img`
+img[start_h:end_h, start_w:end_w] = b
+
+# Place `b_img` into `seg_img`
+seg_img[start_h:end_h, start_w:end_w] = b_img
+
+# Save the results
+cv2.imwrite("./HR-VITON-main/test/test/cloth/00001_00.jpg", img)
+cv2.imwrite("./HR-VITON-main/test/test/cloth-mask/00001_00.jpg", seg_img)
